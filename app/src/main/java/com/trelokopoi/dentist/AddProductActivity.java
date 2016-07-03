a7f782f288a41e8739fb12b09233cb6b6ab844e0
Pl0uk0s <kost.plu@gmail.com>
2016-07-03 19:33:44 +0300

    fixed login, fixed getChildNames, fixed getchild info,fixed children view, sent first version to zoi

--- app/src/main/java/com/trelokopoi/dentist/AddProductActivity.java
+++ app/src/main/java/com/trelokopoi/dentist/AddProductActivity.java
@@ -24,48 +24,6 @@ public class AddProductActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
-
-        String products[] = {"Dell Inspiron", "HTC One X", "HTC Wildfire S",
-                "HTC Sense", "HTC Sensation XE",
-                "iPhone 4S", "Samsung Galaxy Note 800",
-                "Samsung Galaxy S3", "MacBook Air", "Mac Mini", "MacBook Pro"};
-
-        lv = (ListView) findViewById(R.id.list_view);
-        inputSearch = (EditText) findViewById(R.id.search_product);
-
-        adapter = new ArrayAdapter<>(this, R.layout.add_product_list, R.id.product_name, products);
-        lv.setAdapter(adapter);
-
-        /**
-         * Enabling Search Filter
-         * */
-        inputSearch.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
-                // When user changed the Text
-                AddProductActivity.this.adapter.getFilter().filter(cs);
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
-                                          int arg3) {
-                // TODO Auto-generated method stub
-            }
-
-            @Override
-            public void afterTextChanged(Editable arg0) {
-                // TODO Auto-generated method stub
-            }
-        });
-
-        ImageView back = (ImageView) findViewById(R.id.back);
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AddProductActivity.this.onBackPressed();
-            }
-        });
     }
 
     @Override
